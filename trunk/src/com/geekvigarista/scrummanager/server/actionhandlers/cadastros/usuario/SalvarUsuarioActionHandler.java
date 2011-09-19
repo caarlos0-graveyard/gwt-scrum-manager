5220f0a72bfb8cd96fbe3a3eaab8f7a8a081e628
Raduq00@gmail.com <Raduq00@gmail.com@6328de53-f71d-ca59-c669-1449cd93752d>
2011-09-19 15:50:27 +0000

    mudado pacote do action handler salvar usuario

---
+++ trunk/src/com/geekvigarista/scrummanager/server/actionhandlers/cadastros/usuario/SalvarUsuarioActionHandler.java
@@ -0,0 +1,42 @@
+package com.geekvigarista.scrummanager.server.actionhandlers.cadastros.usuario;
+
+import com.geekvigarista.scrummanager.server.guice.DAOModule;
+import com.geekvigarista.scrummanager.server.persistencia.dao.DaoUsuario;
+import com.geekvigarista.scrummanager.shared.commands.usuario.SalvarUsuarioAction;
+import com.geekvigarista.scrummanager.shared.commands.usuario.SalvarUsuarioResult;
+import com.geekvigarista.scrummanager.shared.vos.Usuario;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.gwtplatform.dispatch.server.ExecutionContext;
+import com.gwtplatform.dispatch.server.actionhandler.ActionHandler;
+import com.gwtplatform.dispatch.shared.ActionException;
+
+public class SalvarUsuarioActionHandler implements ActionHandler<SalvarUsuarioAction, SalvarUsuarioResult>
+{
+	
+	Injector inj = Guice.createInjector(new DAOModule());
+	
+	DaoUsuario dao = inj.getInstance(DaoUsuario.class);
+	
+	@Override
+	public SalvarUsuarioResult execute(SalvarUsuarioAction arg0, ExecutionContext arg1) throws ActionException
+	{
+		System.out.println("SalvarUsuarioActionHandler.execute()");
+		Usuario u = dao.salvar(arg0.getUsuario());
+		return new SalvarUsuarioResult(u);
+	}
+	
+	@Override
+	public Class<SalvarUsuarioAction> getActionType()
+	{
+		return SalvarUsuarioAction.class;
+	}
+	
+	@Override
+	public void undo(SalvarUsuarioAction arg0, SalvarUsuarioResult arg1, ExecutionContext arg2) throws ActionException
+	{
+		System.out.println("SalvarUsuarioActionHandler.undo()");
+		
+	}
+	
+}
