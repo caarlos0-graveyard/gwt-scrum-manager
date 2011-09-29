dac8067643566b924051cf0db73852fff0273d23
Raduan Silva dos Santos <raduq00@gmail.com>
2011-09-29 22:36:25 +0000

    adicionado checkbox de administrador no cadastro usuario

--- src/com/geekvigarista/scrummanager/client/converters/UsuarioConverter.java
+++ src/com/geekvigarista/scrummanager/client/converters/UsuarioConverter.java
@@ -5,12 +5,13 @@ import com.google.gwt.user.client.ui.HasValue;
 
 public class UsuarioConverter
 {
-	public static Usuario convert(HasValue<String> nome, HasValue<String> login, HasValue<String> senha)
+	public static Usuario convert(HasValue<String> nome, HasValue<String> login, HasValue<String> senha, HasValue<Boolean> administrador)
 	{
 		Usuario u = new Usuario();
 		u.setNome(nome.getValue());
 		u.setLogin(login.getValue());
 		u.setSenha(login.getValue());
+		u.setAdministrador(administrador.getValue());
 		return u;
 	}
 }
--- src/com/geekvigarista/scrummanager/client/telas/cadastros/usuario/AddUserView.java
+++ src/com/geekvigarista/scrummanager/client/telas/cadastros/usuario/AddUserView.java
@@ -5,6 +5,7 @@ import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.HasValue;
 import com.google.gwt.user.client.ui.Hyperlink;
@@ -31,6 +32,8 @@ public class AddUserView extends ViewImpl implements AddUserPresenter.AddUserVie
 	TextBox senha;
 	@UiField
 	TextBox confSenha;
+	@UiField
+	CheckBox administrador;
 	@UiField
 	Hyperlink btCancelar;
 	@UiField
@@ -81,6 +84,12 @@ public class AddUserView extends ViewImpl implements AddUserPresenter.AddUserVie
 	public Widget asWidget()
 	{
 		return conteudo;
+	}
+
+	@Override
+	public HasValue<Boolean> getAdministrador()
+	{
+		return administrador;
 	}
 	
 }
