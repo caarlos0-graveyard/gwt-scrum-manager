8f0cc7777d0b47b85419608ee777595e41dad9ea
Raduq00@gmail.com <Raduq00@gmail.com@6328de53-f71d-ca59-c669-1449cd93752d>
2011-10-04 13:08:46 +0000

    feito action que verifica se tem usuario logado na sessao

---
+++ trunk/src/com/geekvigarista/scrummanager/shared/commands/usuario/login/VerificaUsuarioLogadoAction.java
@@ -0,0 +1,11 @@
+package com.geekvigarista.scrummanager.shared.commands.usuario.login;
+
+import com.geekvigarista.scrummanager.shared.commands.usuario.buscar.BuscarUsuarioObjResult;
+import com.gwtplatform.dispatch.shared.UnsecuredActionImpl;
+
+public class VerificaUsuarioLogadoAction extends UnsecuredActionImpl<BuscarUsuarioObjResult> 
+{
+
+	public VerificaUsuarioLogadoAction(){}
+	
+}
