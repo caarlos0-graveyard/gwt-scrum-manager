6617152a964c7a6c49dfa5fae4f848068faf6b94
caarlos0 <caarlos0@6328de53-f71d-ca59-c669-1449cd93752d>
2011-10-05 22:17:00 +0000

    criada abstract class extendendo presenter pra fazer os cruds básicos, com handlers e tudo mais, like a boss :)

--- trunk/src/com/geekvigarista/scrummanager/client/telas/cadastros/interfaces/SimpleCadPresenter.java
+++ trunk/src/com/geekvigarista/scrummanager/client/telas/cadastros/interfaces/SimpleCadPresenter.java
@@ -1,17 +1,127 @@
 package com.geekvigarista.scrummanager.client.telas.cadastros.interfaces;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.event.shared.EventBus;
+import com.gwtplatform.mvp.client.Presenter;
+import com.gwtplatform.mvp.client.View;
+import com.gwtplatform.mvp.client.proxy.Proxy;
+
 /**
- * Interface básica a ser implementada pelas presenters de cadastros...
+ * Classe abstrata básica a ser implementada pelas presenters de cadastros...
  * 
  * @author caarlos0
+ * @param <V>
  */
-public interface SimpleCadPresenter
+public abstract class SimpleCadPresenter<V extends View, Proxy_ extends Proxy<?>> extends Presenter<V, Proxy_>
 {
-	void doSalvar();
+	public SalvarHandler salvarHandler = new SalvarHandler();
+	public ExcluirHandler excluirHandler = new ExcluirHandler();
+	public NovoHandler novoHandler = new NovoHandler();
+	public CancelarHandler cancelarHandler = new CancelarHandler();
 	
-	void doNovo();
+	public SimpleCadPresenter(EventBus eventBus, V view, Proxy_ proxy)
+	{
+		super(eventBus, view, proxy);
+	}
 	
-	void doCancelar();
+	public void doSalvar()
+	{
+		try
+		{
+			throw new Exception("você precisa dar override nessa merda");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
 	
-	void doExcluir();
+	public void doNovo()
+	{
+		try
+		{
+			throw new Exception("você precisa dar override nessa merda");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void doCancelar()
+	{
+		try
+		{
+			throw new Exception("você precisa dar override nessa merda");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void doExcluir()
+	{
+		try
+		{
+			throw new Exception("você precisa dar override nessa merda");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * inner classes dos handlers
+	 */
+	
+	public class SalvarHandler implements KeyUpHandler, ClickHandler
+	{
+		@Override
+		public void onClick(ClickEvent event)
+		{
+			doSalvar();
+		}
+		
+		@Override
+		public void onKeyUp(KeyUpEvent event)
+		{
+			if(event.getNativeKeyCode() == KeyCodes.KEY_ENTER)
+			{
+				doSalvar();
+			}
+		}
+	}
+	
+	public class ExcluirHandler implements ClickHandler
+	{
+		@Override
+		public void onClick(ClickEvent event)
+		{
+			doExcluir(); //FIXME mostrar msg confirmacao?
+		}
+	}
+	
+	public class CancelarHandler implements ClickHandler
+	{
+		@Override
+		public void onClick(ClickEvent event)
+		{
+			doCancelar();
+		}
+	}
+	
+	public class NovoHandler implements ClickHandler
+	{
+		@Override
+		public void onClick(ClickEvent event)
+		{
+			doNovo();
+		}
+	}
 }
