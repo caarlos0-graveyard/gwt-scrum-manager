e34df29c23fd264bbaf3f125eaa16bbcecd30327
Carlos Alexandro Becker <caarlos0@gmail.com>
2011-10-05 22:17:00 +0000

    criada abstract class extendendo presenter pra fazer os cruds básicos, com handlers e tudo mais, like a boss :)

--- src/com/geekvigarista/scrummanager/client/telas/cadastros/requisito/AddRequisitoPresenter.java
+++ src/com/geekvigarista/scrummanager/client/telas/cadastros/requisito/AddRequisitoPresenter.java
@@ -8,6 +8,7 @@ import com.geekvigarista.scrummanager.client.converters.interfaces.IRequisitoCon
 import com.geekvigarista.scrummanager.client.gatekeeper.UsuarioLogadoGatekeeper;
 import com.geekvigarista.scrummanager.client.place.NameTokens;
 import com.geekvigarista.scrummanager.client.place.Parameters;
+import com.geekvigarista.scrummanager.client.telas.cadastros.interfaces.SimpleCadPresenter;
 import com.geekvigarista.scrummanager.client.telas.commons.AbstractCallback;
 import com.geekvigarista.scrummanager.client.telas.commons.msgbox.MsgBox;
 import com.geekvigarista.scrummanager.client.telas.componentes.defaultbox.DefaultRichTextArea;
@@ -36,7 +37,6 @@ import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.view.client.SelectionChangeEvent;
 import com.google.gwt.view.client.SingleSelectionModel;
 import com.gwtplatform.dispatch.shared.DispatchAsync;
-import com.gwtplatform.mvp.client.Presenter;
 import com.gwtplatform.mvp.client.View;
 import com.gwtplatform.mvp.client.annotations.NameToken;
 import com.gwtplatform.mvp.client.annotations.ProxyCodeSplit;
@@ -51,7 +51,7 @@ import com.gwtplatform.mvp.client.proxy.RevealContentEvent;
  * @author caarlos0
  * 
  */
-public class AddRequisitoPresenter extends Presenter<AddRequisitoPresenter.AddRequisitoView, AddRequisitoPresenter.AddRequisitoProxy>
+public class AddRequisitoPresenter extends SimpleCadPresenter<AddRequisitoPresenter.AddRequisitoView, AddRequisitoPresenter.AddRequisitoProxy>//Presenter<AddRequisitoPresenter.AddRequisitoView, AddRequisitoPresenter.AddRequisitoProxy>
 {
 	
 	@ProxyCodeSplit
@@ -134,11 +134,10 @@ public class AddRequisitoPresenter extends Presenter<AddRequisitoPresenter.AddRe
 	protected void onBind()
 	{
 		super.onBind();
-		SalvarRequisitoHandler handler = new SalvarRequisitoHandler();
-		getView().getBtSalvar().addClickHandler(handler);
-		getView().prioridade().addKeyUpHandler(handler);
-		getView().titulo().addKeyUpHandler(handler);
-		getView().tempoEstimado().addKeyUpHandler(handler);
+		getView().getBtSalvar().addClickHandler(salvarHandler);
+		getView().prioridade().addKeyUpHandler(salvarHandler);
+		getView().titulo().addKeyUpHandler(salvarHandler);
+		getView().tempoEstimado().addKeyUpHandler(salvarHandler);
 		getView().getBtVoltar().addClickHandler(new ClickHandler()
 		{
 			@Override
@@ -276,6 +275,7 @@ public class AddRequisitoPresenter extends Presenter<AddRequisitoPresenter.AddRe
 		placeManager.revealPlace(pr);
 	}
 	
+	@Override
 	public void doSalvar()
 	{
 		Requisito req = converter.convert(getRequisito(), getView());
@@ -299,24 +299,6 @@ public class AddRequisitoPresenter extends Presenter<AddRequisitoPresenter.AddRe
 				}
 			}
 		});
-	}
-	
-	private class SalvarRequisitoHandler implements ClickHandler, KeyUpHandler
-	{
-		
-		@Override
-		public void onKeyUp(KeyUpEvent event)
-		{
-			if(event.getNativeKeyCode() == KeyCodes.KEY_ENTER)
-				doSalvar();
-		}
-		
-		@Override
-		public void onClick(ClickEvent event)
-		{
-			doSalvar();
-		}
-		
 	}
 	
 	@Override
