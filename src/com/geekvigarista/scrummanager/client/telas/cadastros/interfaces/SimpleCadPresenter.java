3b2517714b7a669d7bbf7bce72b127bc799cf31e
Carlos Alexandro Becker <caarlos0@gmail.com>
2011-10-05 21:36:58 +0000

    interface padrao para os presenters..

---
+++ src/com/geekvigarista/scrummanager/client/telas/cadastros/interfaces/SimpleCadPresenter.java
@@ -0,0 +1,17 @@
+package com.geekvigarista.scrummanager.client.telas.cadastros.interfaces;
+
+/**
+ * Interface básica a ser implementada pelas presenters de cadastros...
+ * 
+ * @author caarlos0
+ */
+public interface SimpleCadPresenter
+{
+	void doSalvar();
+	
+	void doNovo();
+	
+	void doCancelar();
+	
+	void doExcluir();
+}
