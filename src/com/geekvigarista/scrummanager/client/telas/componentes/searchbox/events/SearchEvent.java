1e660acaeacd537c2b9a65438ca83652343c6526
Carlos Alexandro Becker <caarlos0@gmail.com>
2011-10-08 22:19:02 +0000

    milhoes de alteracoes

---
+++ src/com/geekvigarista/scrummanager/client/telas/componentes/searchbox/events/SearchEvent.java
@@ -0,0 +1,36 @@
+package com.geekvigarista.scrummanager.client.telas.componentes.searchbox.events;
+
+import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.event.shared.GwtEvent;
+
+public class SearchEvent extends GwtEvent<SearchEventHandler>
+{
+	
+	private static final Type<SearchEventHandler> TYPE = new Type<SearchEventHandler>();
+	
+	public static Type<SearchEventHandler> getType()
+	{
+		return TYPE;
+	}
+	
+	public static void fire(EventBus eventBus)
+	{
+		eventBus.fireEvent(new SearchEvent());
+	}
+	
+	public SearchEvent()
+	{
+	}
+	
+	@Override
+	protected void dispatch(SearchEventHandler handler)
+	{
+		handler.onSearch(this);
+	}
+	
+	@Override
+	public Type<SearchEventHandler> getAssociatedType()
+	{
+		return getType();
+	}
+}
---
+++ src/com/geekvigarista/scrummanager/client/telas/componentes/searchbox/events/SearchEventHandler.java
@@ -0,0 +1,8 @@
+package com.geekvigarista.scrummanager.client.telas.componentes.searchbox.events;
+
+import com.google.gwt.event.shared.EventHandler;
+
+public interface SearchEventHandler extends EventHandler
+{
+	void onSearch(SearchEvent event);
+}
